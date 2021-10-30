38dcd126060a0411d1b38f7040363f9d3415ba17
riya40 <priyanunna20@gmail.com>
2021-10-30 08:35:25 +0530

    Added:insertion sort value

--- src/com/bridgelabz/algorithms/InsertionSort.java
+++ src/com/bridgelabz/algorithms/InsertionSort.java
@@ -1,1 +1,33 @@
+/**
+ * Printing and Sorting the String Values
+ * Using the Insertion sort
+ */
+package com.bridgelabz.algorithms;
 
+public class InsertionSort {
+    public void sorting(String arr[], int n){
+
+        for(int i=0;i<n;++i){
+            for(int j=i+1;j<n;j++ ) {
+                if(arr[i].compareToIgnoreCase(arr[j])>0) {
+                    String temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+    public void print(String arr[],int f){
+        f=arr.length;
+        for(int i=0;i<f;i++){
+            System.out.println(arr[i]);
+        }
+    }
+    public static void main(String args[]){
+        InsertionSort insertionsort=new InsertionSort();
+        String arr[]={"riya","sankar","jenny","chris"};
+        int n=arr.length;
+        insertionsort.sorting(arr,n);
+        insertionsort.print(arr,n);
+    }
+}
